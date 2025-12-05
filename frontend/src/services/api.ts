c0e70bc6fb9be1573c1dbf5bd957f810ee0e8261
andressaamaro495 <andressaamaro495@gmail.com>
2025-12-05 10:23:52 +0000

    AAAAA

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { User, Pet, Consulta, Vacina, CarteiraSaude } from '../../types';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://clinica-patinhas-socorro.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
